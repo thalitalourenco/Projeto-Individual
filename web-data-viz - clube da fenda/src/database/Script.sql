982aa4f1a30614a4b46deb6a4b79321073e1b392
thalitalourenco <thalita.lourenco@sptech.school>
2025-06-01 12:49:32 -0300

    Alterações na tela inicial, adicionei uma foto minha criança e um missão,visão e valores

--- web-data-viz - clube da fenda/src/database/Script.sql
+++ web-data-viz - clube da fenda/src/database/Script.sql
@@ -2,38 +2,86 @@ create database clube_da_fenda;
 
 use clube_da_fenda;
 
-show tables;
-
-create table usuarios(
+create table usuario(
 id int primary key auto_increment,
 nome varchar(45),
-email varchar(100),
-telefone char(11),
+email varchar(100) unique,
+telefone char(15),
 senha varchar(50),
 data_cadastro datetime default current_timestamp
 );
 
-create table partidas(
+create table jogo(
 id int primary key auto_increment,
-user_id int,
-pontuacao int,
-data_partida datetime default current_timestamp,
-resultado varchar(20),
-constraint resultado_partida 
-    check (resultado in ('Vitória', 'Derrota')),
-constraint fkUserPartida
-    foreign key (user_id)
-    references user(id)
+nome varchar(45)
 );
 
-create table estatisticas(
-id int primary key auto_increment,
-user_id int,
-total_partidas int,
-media_pontuacao float,
-melhor_pontuacao int,
-data_ultima_partida datetime,
-constraint fkUserEstatisticas
-    foreign key (user_id)
-    references user(id)
+create table partida(
+idPartida int auto_increment,
+idUser int,
+idJogo int,
+pontuacao int,
+tempo int,
+personagem varchar(45),
+data_partida datetime default current_timestamp,
+primary key(idPartida, idUser, idJogo),
+constraint fkUser
+    foreign key (idUser)
+    references usuario(id),
+constraint fkJogo
+    foreign key (idJogo)
+    references jogo(id)
 );
+
+insert into jogo values
+(default, 'Quiz'),
+(default, 'Quiz de Personalidade'),
+(default, 'Cruzadinha'),
+(default, 'Gary Doodle'); 
+
+select * from usuario;
+
+create view rankingPersonagens as
+select 
+    personagem, 
+    count(*) as quantidade
+from 
+    partida
+where 
+    personagem is not null and personagem <> ''
+group by 
+    personagem
+order by 
+    quantidade desc;
+    
+create view rankingUsuarios as
+select 
+    u.nome,
+    count(p.idPartida) as quantidadePartidas
+from 
+    usuario u
+left join 
+    partida p ON u.id = p.idUser
+group by 
+    u.id, u.nome
+order by 
+    quantidadePartidas desc
+limit 5;
+
+alter view rankingUsuarios as 
+select 
+    u.nome,
+    count(p.idPartida) as quantidadePartidas
+from 
+    usuario u
+left join 
+    partida p ON u.id = p.idUser
+group by 
+    u.id, u.nome
+order by 
+    quantidadePartidas desc
+limit 3;
+
+select nome from rankingUsuarios;
+    
+select personagem, quantidade from rankingPersonagens;
